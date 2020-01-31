f84f90760cbc9b80cc38f1924b70e2b339d60e75
dan-codes <dli123@syr.edu>
2020-01-31 14:50:20 -0500

    first commit

---
+++ miningTweets.py
@@ -0,0 +1,16 @@
+import twitter
+import json
+import os
+
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_SECRET_TOKEN')
+api = twitter.Api(consumer_key=consumer_key,
+                  consumer_secret=consumer_secret,
+                  access_token_key=access_token,
+                  access_token_secret=access_token_secret)
+print(api.VerifyCredentials())
+
+users = api.GetFriends()
+print(users)

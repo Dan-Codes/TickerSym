4534753406a879397530f07b0a61b533d4a7f210
dan-codes <dli123@syr.edu>
2020-04-14 23:41:44 -0400

    mining current tweets

--- miningTweets.py
+++ miningTweets.py
@@ -1,6 +1,9 @@
 import twitter
 import json
 import os
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 
 consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_secret = os.environ.get('CONSUMER_SECRET')
@@ -12,5 +15,53 @@ api = twitter.Api(consumer_key=consumer_key,
                   access_token_secret=access_token_secret)
 print(api.VerifyCredentials())
 
-users = api.GetFriends()
-print(users)
+
+class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets.
+    """
+
+    def __init__(self):
+        pass
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        # This handles Twitter authentication and the connection to Twitter Streaming API
+        listener = StdOutListener(fetched_tweets_filename)
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        stream = Stream(auth, listener)
+
+        # This line filter Twitter Streams to capture data by the keywords:
+        stream.filter(track=hash_tag_list)
+
+
+# # # # TWITTER STREAM LISTENER # # # #
+class StdOutListener(StreamListener):
+    """
+    This is a basic listener that just prints received tweets to stdout.
+    """
+
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+
+    def on_data(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+    # Authenticate using config.py and connect to Twitter Streaming API.
+    hash_tag_list = ["S&P500", "SP500", "$SPX", "$SPY"]
+    fetched_tweets_filename = "tweets.txt"
+
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
